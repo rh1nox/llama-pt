a5adbf80d52ca63b0c51dac9f2ec62f184f79765
motngr <motngr@hacker.com>
2024-04-29 10:31:56 +0800

    test

--- run_clm_pt.py
+++ run_clm_pt.py
@@ -603,7 +603,7 @@ def main():
             torch_dtype=torch_dtype,
             low_cpu_mem_usage=True,
             device_map=device_map,
-            load_in_8bit=True,
+            #load_in_8bit=True,
             quantization_config=quantization_config,
             use_flash_attention_2=training_args.use_flash_attention_2
         )
